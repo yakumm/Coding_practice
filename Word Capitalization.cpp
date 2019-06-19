cc6070ed60e1f6df61d4db905eff174c00fcabd1
Yash Kumar <49211982+yakumm@users.noreply.github.com>
2019-06-19 13:50:11 +0530

    Create Word Capitalization.cpp

---
+++ Word Capitalization.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string su;
+    cin>>su;
+    if(isupper(su[0])==true)
+    {
+        cout<<su<<endl;1
+    }
+    else
+    {
+        su[0]=su[0]-32;
+        cout<<su<<endl;
+    }
+}
+
